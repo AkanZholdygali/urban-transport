06b2b1bb95814c4907d3b5b256741c99b2417e78
AkanZholdygali <31376@iitu.edu.kz>
2024-05-05 22:27:13 +0500

    fixed TransportController.java

--- src/main/java/kz/akan/springcourse/urbantransport/controller/TransportController.java
+++ src/main/java/kz/akan/springcourse/urbantransport/controller/TransportController.java
@@ -5,6 +5,7 @@ import kz.akan.springcourse.urbantransport.model.TransportType;
 import kz.akan.springcourse.urbantransport.repository.TransportRepository;
 import kz.akan.springcourse.urbantransport.repository.TransportTypeRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class TransportController {
     private final TransportRepository transportRepository;
     private final TransportTypeRepository transportTypeRepository;
