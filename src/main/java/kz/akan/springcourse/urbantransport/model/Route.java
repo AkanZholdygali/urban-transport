56b9fd988371fca7c4b71007c152b2201891c6f7
AkanZholdygali <31376@iitu.edu.kz>
2024-05-05 14:19:47 +0500

    fixed json reference

--- src/main/java/kz/akan/springcourse/urbantransport/model/Route.java
+++ src/main/java/kz/akan/springcourse/urbantransport/model/Route.java
@@ -1,7 +1,6 @@
 package kz.akan.springcourse.urbantransport.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
